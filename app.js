5aefb69613820688e0c21a244a3806ab88523c2b
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-08-08 17:04:57 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import {seccion} from './seccion-creator.js'
+
+const root = document.getElementById('root')
+
+const seccion_pagar_facturas = seccion('pagarFactura', 'secciones', 'Paga tus facturas')
+const seccion_comprar_internet = seccion('comprarInternet', 'secciones', 'Compra tu Internet')
+const seccion_comprar_roaming = seccion('comprarRoaming', 'secciones', 'Compra roaming')
+const seccion_recargar = seccion('recargar', 'secciones', 'Recargar propias y a terceros')
+const seccion_consulta_consumo = seccion('consulta_consumo', 'secciones', 'Consultar consumo')
+
+root.appendChild(seccion_pagar_facturas)
+root.appendChild(seccion_comprar_internet)
+root.appendChild(seccion_comprar_roaming)
+root.appendChild(seccion_recargar)
+root.appendChild(seccion_consulta_consumo)
---
+++ seccion-creator.js
@@ -0,0 +1,12 @@
+export const seccion = (id, clase, text) => {
+    const element = document.createElement('div')
+    element.id = id
+    element.classList.add(clase)
+    element.textContent = text
+    element.addEventListener('click', mensaje)
+    return element
+}
+
+const mensaje = (idSeccion) => {
+    alert(`klk con klk ${idSeccion}`)
+}
