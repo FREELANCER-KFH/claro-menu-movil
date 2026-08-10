55dca91cdd1809bcc6f61c5732f357066549f2fa
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-08-10 22:38:12 +0000

    probando el creador de secciones

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const seccion_comprar_internet = seccion('comprarInternet', 'secciones', 'Compra
 const seccion_comprar_roaming = seccion('comprarRoaming', 'secciones', 'Compra roaming')
 const seccion_recargar = seccion('recargar', 'secciones', 'Recargar propias y a terceros')
 const seccion_consulta_consumo = seccion('consulta_consumo', 'secciones', 'Consultar consumo')
+const seccion_reclamacion = seccion('reclamo', 'secciones', 'Realiza tus reclamos aqui')
 
 root.appendChild(seccion_pagar_facturas)
 root.appendChild(seccion_comprar_internet)
 root.appendChild(seccion_comprar_roaming)
 root.appendChild(seccion_recargar)
 root.appendChild(seccion_consulta_consumo)
+root.appendChild(seccion_reclamacion)
